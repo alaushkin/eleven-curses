a2f42388fc22ce4c341713d987d05417fa202d1d
Mak <alaushkin3@gmail.com>
2015-03-23 22:04:37 +0300

    оценка пользователей

--- Diplom11/app/src/main/java/com/diplom11/diplom11/UserInfo.java
+++ Diplom11/app/src/main/java/com/diplom11/diplom11/UserInfo.java
@@ -4,10 +4,13 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
 import com.diplom11.diplom11.UserTools.User;
+import com.parse.ParseACL;
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
@@ -20,7 +23,8 @@ import java.util.List;
 public class UserInfo extends ActionBarActivity {
     private TextView usInfo;
     private RatingBar usRatingBar;
-
+    private Button usVote;
+    //Assessment
     private User user;
 
     @Override
@@ -56,8 +60,9 @@ public class UserInfo extends ActionBarActivity {
     private void init(){
         usInfo = (TextView) findViewById(R.id.usInfo);
         usRatingBar = (RatingBar) findViewById(R.id.usRatingBar);
+        usVote = (Button) findViewById(R.id.usVote);
 
-        String userId = getIntent().getStringExtra("userId");
+        final String userId = getIntent().getStringExtra("userId");
         ParseQuery query = new ParseQuery("UserData");
         query.whereEqualTo("userId", userId);
         try {
@@ -67,22 +72,72 @@ public class UserInfo extends ActionBarActivity {
                 usInfo.setText(user.toString());
                 if(user.getUserId().equals(ParseUser.getCurrentUser().getObjectId())){
                     usRatingBar.setEnabled(false);
+                    usVote.setEnabled(false);
                 }
+                getRating();
             } else{
                 usInfo.setText("Пользователь не найден");
                 usRatingBar.setEnabled(false);
+                usVote.setEnabled(false);
             }
         } catch (ParseException e) {
             usInfo.setText("Произошла непредвиденная ошибка");
             usRatingBar.setEnabled(false);
+            usVote.setEnabled(false);
             return;
         }
 
-        usRatingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+        usVote.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
-
+            public void onClick(View v) {
+                ParseObject parseObject = new ParseObject("Assessment");
+                parseObject.put("User1", ParseUser.getCurrentUser().getObjectId());
+                parseObject.put("User2", user.getUserId());
+                parseObject.put("value", usRatingBar.getRating());
+                ParseACL acl = new ParseACL();
+                acl.setPublicReadAccess(true);
+                acl.setPublicWriteAccess(true);
+                try {
+                    parseObject.save();
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+                ParseQuery parseQuery = new ParseQuery("UserData");
+                parseQuery.whereEqualTo("userId", user.getUserId());
+                try {
+                    List list = parseQuery.find();
+                    if(list.size() > 0){
+                        ParseObject usr = (ParseObject)list.get(0);
+                        double sumReit = usr.getDouble("sumReit");
+                        int count = usr.getInt("voteCount");
+                        sumReit += usRatingBar.getRating()*2;
+                        count++;
+                        usr.put("sumReit", sumReit);
+                        usr.put("voteCount", count);
+                        usr.save();
+                    }
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+                startActivity(getIntent());
             }
         });
+    }
+
+    public void getRating(){
+        ParseQuery parseQuery = new ParseQuery("Assessment");
+        parseQuery.whereEqualTo("User1", ParseUser.getCurrentUser().getObjectId());
+        parseQuery.whereEqualTo("User2", user.getUserId());
+        try {
+            List list = parseQuery.find();
+            if(list.size() > 0){
+                usRatingBar.setEnabled(false);
+                usVote.setEnabled(false);
+                Double value = ((ParseObject)list.get(0)).getDouble("value");
+                usRatingBar.setRating(value.floatValue());
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
     }
 }
