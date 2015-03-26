87a515f42a05cc75b681360becc5a41c0267d2be
Viacheslav Egorenkov <ve@poloniumarts.com>
2015-03-26 16:57:42 +0300

    [refactor]Route fragment

--- Diplom11/app/src/main/java/com/diplom11/diplom11/Route.java
+++ Diplom11/app/src/main/java/com/diplom11/diplom11/Route.java
@@ -1,27 +1,22 @@
 package com.diplom11.diplom11;
 
 import android.support.v4.app.FragmentActivity;
-import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.FragmentById;
 
+@EActivity(R.layout.activity_route)
 public class Route extends FragmentActivity {
-    SupportMapFragment mapFragment;
+    @FragmentById SupportMapFragment mapFragment;
     GoogleMap map;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_route);
+    @AfterViews void init() {
         GooglePlayServicesUtil.isGooglePlayServicesAvailable(getApplicationContext());
-        mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
         map = mapFragment.getMap();
         if (map == null) {
             finish();
